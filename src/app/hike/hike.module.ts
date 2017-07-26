1c33d27d2907f9c6e141544d6b79c80e1228dbc6
Julian Amet <julian.amet@adp.com>
2017-07-26 17:14:03 +0200

    #Le routage avancé - 2.Créer un lien incluant un paramètre

--- src/app/hike/hike.module.ts
+++ src/app/hike/hike.module.ts
@@ -1,11 +1,12 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {RouterModule} from '@angular/router';
 import {HikeListComponent} from './hike-list.component';
 import {HikeDetailsComponent} from './hike-details.component';
 import {HikeService} from './hike.service';
 
 @NgModule({
-    imports: [CommonModule],
+    imports: [CommonModule, RouterModule],
     declarations: [HikeListComponent, HikeDetailsComponent],
     exports:[HikeListComponent],
     providers:[HikeService]
