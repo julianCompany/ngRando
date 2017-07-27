d847fa4967824db0cba021d76a0b3285dc261ef0
Julian Amet <julian.amet@adp.com>
2017-07-27 12:21:17 +0200

    #Utiliser les pipes existants et créer un custom pipe : 2.Le two way binding à l'aide de [(ngmodel)]

--- src/app/hike/hike.module.ts
+++ src/app/hike/hike.module.ts
@@ -4,9 +4,10 @@ import {RouterModule} from '@angular/router';
 import {HikeListComponent} from './hike-list.component';
 import {HikeDetailsComponent} from './hike-details.component';
 import {HikeService} from './hike.service';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
-    imports: [CommonModule, RouterModule],
+    imports: [CommonModule, RouterModule, FormsModule],
     declarations: [HikeListComponent, HikeDetailsComponent],
     exports:[HikeListComponent],
     providers:[HikeService]
