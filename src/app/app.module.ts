65dae87789a740a8d35b5482c7953ad65175db42
Julian Amet <julian.amet@adp.com>
2017-07-28 11:59:30 +0200

    #Créer et gérer un formulaire : 1.Créer notre nouveau module “contact.module.ts”

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,16 +9,20 @@ import { HomeComponent} from './home/home.component';
 import { HikeListComponent} from './hike/hike-list.component';
 import { PageNotFoundComponent} from './page-not-found.component';
 import { HikeDetailsComponent} from './hike/hike-details.component';
+import {ContactUsComponent} from './contact/contact-us.component';
+import {ContactModule} from './contact/contact.module';
 
 @NgModule({
   imports:      [ BrowserModule,
                   HikeModule,
                   HttpModule,
                   HomeModule,
+                  ContactModule,
                   RouterModule.forRoot([
                     {path : 'home', component : HomeComponent},
                     {path : 'hikes', component : HikeListComponent},
                     {path : 'hike/:id', component :HikeDetailsComponent},
+                    {path : 'contact', component :ContactUsComponent},
                     {path : '', redirectTo : 'home', pathMatch :'full'},
                     {path : '**', component : PageNotFoundComponent}
                   ])],
---
+++ src/app/contact/contact-us.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+    moduleId : module.id,
+    templateUrl: 'contact-us.component.html'
+})
+
+export class ContactUsComponent {
+    
+}
